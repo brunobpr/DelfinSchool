092cfc2a7276b20d0359209e47fb512e8807269c
brunobpr <brunopr.bpr@gmail.com>
2017-04-09 06:42:47 +0100

    Initial commit

---
+++ app/src/main/java/app/getitright/com/example/android/delfinschool/MainActivity.java
@@ -0,0 +1,49 @@
+package app.getitright.com.example.android.delfinschool;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void facebook(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.facebook.com/delfinenglishschool/"));
+        startActivity(intent);
+    }
+
+    public void instagram(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.instagram.com/delfinschool/"));
+        startActivity(intent);
+    }
+
+    public void youtube(View view) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.youtube.com/user/delfinenglishschool"));
+        startActivity(intent);
+    }
+
+    public void website(View view){
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+        intent.setData(Uri.parse("https://www.delfinschool.com"));
+        startActivity(intent);
+    }
+}
